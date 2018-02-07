0ad4159a7e74e62078015ba15a008f5b3afe6c74
Nikit Bobba <nikitbobba2019@u.northwestern.edu>
2018-02-07 17:33:18 -0600

    Nikit - Add left/right movement
    Added left/right movement for player

---
+++ StarCats/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Transform player;
+    public float speed;
+	public float maxBound, minBound;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float h = Input.GetAxis("Horizontal"); //uses the A&D key
+
+		if (player.position.x < minBound && h < 0)
+		{
+			h = 0;
+		}
+		else if (player.position.x > maxBound && h > 0)
+		{
+			h = 0;
+		}
+
+		player.position += Vector3.right * h * speed;
+
+
+
+	}
+}
