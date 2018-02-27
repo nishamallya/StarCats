6edbc7a98ff16d9639cc0995522c78bcc74b4453
Nikit Bobba <nikitbobba2019@u.northwestern.edu>
2018-02-27 14:03:54 -0600

    Added Main Menu

---
+++ StarCats/Assets/RotatePlanet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlanet : MonoBehaviour
+{
+
+	public float rotationsPerMinute = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(6.0f*rotationsPerMinute*Time.deltaTime,0,0);
+		
+	}
+}
